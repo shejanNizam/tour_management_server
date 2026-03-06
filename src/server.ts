356cb78c3ce5f0e454e87da9f796c1d557474b2b
nizam <nizam.shejan11@gmail.com>
2026-03-06 15:47:10 +0600

    unhandled globar error exit process

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { configs } from "./app/config";
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(configs.database_url as string);
 
-    app.listen(configs.port, () => {
+    console.log("Connected to DB!");
+
+    server = app.listen(configs.port, () => {
       console.log(`Tour Management Server is running on port ${configs.port}`);
     });
   } catch (error) {
@@ -15,3 +20,50 @@ async function main() {
 }
 
 main();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal recieved... Server shutting down..");
+
+  if (server) {
+    process.exit(1);
+  }
+  process.exit(1);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal recieved... Server shutting down..");
+
+  if (server) {
+    process.exit(1);
+  }
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+  if (server) {
+    process.exit(1);
+  }
+  process.exit(1);
+});
+// Unhandler rejection error
+// Promise.reject(new Error("I forgot to catch this promise"));
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down..", err);
+
+  if (server) {
+    process.exit(1);
+  }
+  process.exit(1);
+});
+
+// Uncaught Exception Error
+// throw new Error("I forgot to handle this local erro");
+
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
