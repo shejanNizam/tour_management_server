3de78ca2fd1df02229ba16e5db53143c9320ee68
nizam <nizam.shejan11@gmail.com>
2026-03-12 08:47:44 +0600

    create user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+
+export enum IsActive {
+  ACTICE = "ACTICE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+export interface IAuthProvider {
+  provider: string; // Google, Credential(email + password)
+  providerId: string;
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified: string;
+  role: Role;
+  auths: IAuthProvider;
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
